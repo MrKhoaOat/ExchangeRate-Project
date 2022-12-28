fccecefe114d71c6af35b75ea1feb0c0520d3208
MrKhoaOat 6452300113 <109195575+MrKhoaOat@users.noreply.github.com>
2022-12-28 09:02:30 +0700

    Add files via upload

---
+++ src/configs/constants.js
@@ -0,0 +1,19 @@
+//DATABASE 
+const DB_Host = 'localhost';
+const DB_User = 'root';
+const DB_Password = '1234';
+const DB_Database = 'Project';
+const DB_Port = 3306;
+
+
+//SEVER_PORT
+const SERVER_PORT = 4500;
+
+module.exports = {
+ DB_Host,
+ DB_User,
+ DB_Password,
+ DB_Database,
+ DB_Port,
+ SERVER_PORT
+}
